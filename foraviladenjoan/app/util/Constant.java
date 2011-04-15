d1ee78dcaa5c3143e4dc04d530cd33dafce7d465
antonio.maria.sanchez@gmail.com <antonio.maria.sanchez@gmail.com@f1afad12-8c15-7cb2-3e06-89236ed1fdf0>
2011-04-15 08:01:27 +0000

    Aplicacion utilizando Play Framework

---
+++ foraviladenjoan/app/util/Constant.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Constant {
+	
+	public static final String IMAGES_BASE_PATH = "images/";
+
+	public static final String VEGETABLE = "vegetable";
+	
+	public static final String MISCELLANY = "miscellany";
+	
+	public static final String FRUIT = "fruit";
+	
+	private Constant(){
+		
+	}
+}
